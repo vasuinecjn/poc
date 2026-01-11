5aad752167440ef98413132871477cc5b8f04ffa
Srinivas JN <vasuinecjn@gmail.com>
2026-01-11 17:42:29 +0530

    code cleaning

--- src/main/java/org/poc/pages/PayPalHomePage.java
+++ src/main/java/org/poc/pages/PayPalHomePage.java
@@ -1,6 +1,5 @@
 package org.poc.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
