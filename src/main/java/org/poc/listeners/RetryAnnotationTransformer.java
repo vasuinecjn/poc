583be1f6a265c405e041f1ee48e418fe958621f5
Srinivas JN <vasuinecjn@gmail.com>
2026-01-13 12:43:37 +0530

    Correcting the name of the test

--- src/main/java/org/poc/listeners/RetryAnnotationTransformer.java
+++ src/main/java/org/poc/listeners/RetryAnnotationTransformer.java
@@ -1,7 +1,6 @@
 package org.poc.listeners;
 
 import org.testng.IAnnotationTransformer;
-import org.testng.annotations.IFactoryAnnotation;
 import org.testng.annotations.ITestAnnotation;
 
 import java.lang.reflect.Constructor;
@@ -10,7 +9,10 @@ import java.lang.reflect.Method;
 public class RetryAnnotationTransformer implements IAnnotationTransformer {
 
     @Override
-    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+    public void transform(ITestAnnotation annotation,
+                          Class testClass,
+                          Constructor testConstructor,
+                          Method testMethod) {
         if (annotation.getRetryAnalyzerClass() == null) {
             annotation.setRetryAnalyzer(SeleniumRetryAnalyzer.class);
         }
